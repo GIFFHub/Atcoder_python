35574d1483fa52427510d32149c8c3648e20ce2a
GIFFHub <yusuke94ka2nakano@gmail.com>
2022-10-07 10:24:02 +0900

    2022-10-07-01

--- 01_ABC/253/C.py
+++ 01_ABC/253/C.py
@@ -1,2 +1,15 @@
 
+Q = int(input())
+S = []
 
+for _ in range(Q):
+    query = list(map(int, input().split()))
+
+    if query[0] == 1:
+        S.append(query[1])
+
+    elif query[0] == 2:
+        pass
+
+    else:
+        print(max(S) - min(S))
