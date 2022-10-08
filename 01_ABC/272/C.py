dd60f93f9b9d30a68a7b07eaaa3bbb6443750608
GIFFHub <yusuke94ka2nakano@gmail.com>
2022-10-08 23:15:50 +0900

    2022-10-08-01

--- 01_ABC/272/C.py
+++ 01_ABC/272/C.py
@@ -2,52 +2,35 @@
 
 N = int(input())
 A = list(map(int, input().split()))
+ans = -1
 
-g_cnt = 0
-k_cnt = 0
-for i in range(N):
-    if A[i] % 2 == 0:
-        g_cnt += 1
+A.sort(reverse=True)
+
+if A[0] % 2 == 0:
+    if A[1] % 2 == 0:
+        ans = A[0]+A[1]
+        print(ans)
+        exit()
     else:
-        k_cnt += 1
+        for i in range(2, N):
+            if A[i] % 2 == 0:
+                ans = max(ans, A[0]+A[i])
+            else:
+                ans = max(ans, A[1]+A[i])
 
-ans1 = -1
-ans2 = -1
-ans3 = -1
-ans4 = -1
-ans5 = -1
-ans6 = -1
+elif A[0] % 2 == 1:
+    if A[1] % 2 == 1:
+        ans = A[0]+A[1]
+        print(ans)
+        exit()
+    else:
+        for i in range(2, N):
+            if A[i] % 2 == 0:
+                ans = max(ans, A[1]+A[i])
+            else:
+                ans = max(ans, A[0]+A[i])
 
-if g_cnt < 2 and k_cnt < 2:
-    print(-1)
-else:
-    A.sort(reverse=True)
-
-    if A[0] % 2 == 0:
-        if A[1] % 2 == 0:
-            ans1 = A[0]+A[1]
-            print(ans1)
-            exit()
-        else:
-            for i in range(2, N):
-                if A[i] % 2 == 0:
-                    ans2 = max(ans2, A[0]+A[i])
-                else:
-                    ans3 = max(ans3, A[1]+A[i])
-
-    elif A[0] % 2 == 1:
-        if A[1] % 2 == 1:
-            ans4 = A[0]+A[1]
-            print(ans4)
-            exit()
-        else:
-            for i in range(2, N):
-                if A[i] % 2 == 0:
-                    ans5 = max(ans5, A[1]+A[i])
-                else:
-                    ans6 = max(ans6, A[0]+A[i])
-
-    print(max(ans1, ans2, ans3, ans4, ans5, ans6))
+print(ans)
 
 
 
