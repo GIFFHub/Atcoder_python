7c48291d798626f0b9f762039a4ad34f7596ad93
GIFFHub <yusuke94ka2nakano@gmail.com>
2022-10-14 20:36:41 +0900

    2022-10-13-01

--- 01_ABC/245/C.py
+++ 01_ABC/245/C.py
@@ -2,22 +2,26 @@ N, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+dp_A = [False]*N
+dp_B = [False]*N
 
-pos = 0
-for i in range(N):
+dp_A[0] = True
+dp_B[0] = True
 
-    if i == 0:
-        if max(A[i], B[i]) < K:
-            pos = max(A[i], B[i])
-        else:
-            pos = min(A[i], B[i])
-    else:
-        if max(A[i], B[i]) - pos < K:
-            pos = max(A[i], B[i])
-        elif min(A[i], B[i]) - pos < K:
-            print('No')
-            exit()
-        else:
-            pos = min(A[i], B[i])
 
-print('Yes')
+for i in range(1, N):
+    if dp_A[i-1]:
+        if abs(A[i] - A[i-1]) <= K:
+            dp_A[i] = True
+        if abs(B[i] - A[i-1]) <= K:
+            dp_B[i] = True
+    if dp_B[i-1]:
+        if abs(A[i] - B[i-1]) <= K:
+            dp_A[i] = True
+        if abs(B[i] - B[i-1]) <= K:
+            dp_B[i] = True
+
+if dp_A[N-1] or dp_B[N-1]:
+    print('Yes')
+else:
+    print('No')
