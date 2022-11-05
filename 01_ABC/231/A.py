ad7403c7c7ad81b00bb2d5f0d6183a4806552869
GIFFHub <yusuke94ka2nakano@gmail.com>
2022-11-06 08:36:30 +0900

    2022-11-07-01

---
+++ 01_ABC/231/A.py
@@ -0,0 +1,5 @@
+
+
+if __name__ == '__main__':
+    D = int(input())
+    print(D/100)
--- 01_ABC/276/D.py
+++ 01_ABC/276/D.py
@@ -1,2 +1,39 @@
+import math
+from functools import reduce
+
+def my_gcd(*numbers):
+    return reduce(math.gcd, numbers)
+
 
 if __name__ == '__main__':
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    min_A = my_gcd(*A)
+    ans = 0
+    for a in A:
+        if a % min_A != 0:
+            print(-1)
+            exit()
+        else:
+            if a == min_A:
+                continue
+
+            tmp = a//min_A
+
+            if tmp % 2 != 0 and tmp % 3 != 0:
+                print(-1)
+                exit()
+            while tmp >= 2 and tmp % 2 == 0:
+                ans += 1
+                tmp //= 2
+            while tmp >= 3 and tmp % 3 == 0:
+                ans += 1
+                tmp //= 3
+
+            if tmp != 1:
+                print(-1)
+                exit()
+
+    print(ans)
+
