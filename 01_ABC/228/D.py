16ff1d3dc582811a83391d71a5878d03b11993e0
GIFFHub <yusuke94ka2nakano@gmail.com>
2022-12-01 05:44:44 +0900

    2022-12-01-01

--- 01_ABC/228/D.py
+++ 01_ABC/228/D.py
@@ -1,24 +1,44 @@
+import sys
+sys.setrecursionlimit(1<<30)
+
+
+def index(x):
+    '''
+
+    :param x: index
+    :return: indexから最も近い-1となっているindex（自身を含む）
+    '''
+
+    h = x % n # 問題の指定の通りxをhに変換する
+
+    # 既に辞書cntにindex: hが登録されている場合：すでに-1ではなくなっている
+    if h in cnt:
+        # cnt[h]は、else文でh+1に、もしくは本ループでさらに更新している可能性がある
+        cnt[h] = index(cnt[h])
+        return cnt[h]
+    # 登録されていない場合：returnはそのままhでいい
+    else:
+        # cnt[h]はhではなく、h+1とする（hは-1ではなくなるから）
+        # h+1が-1でなかったら？
+        # 次hにアクセスする時、このelifにはこない
+        # 上のifにいき、
+        cnt[h] = (h+1) % n
+        return h
 
 
 if __name__ == '__main__':
-    Q = int(input())
-    T = []
-    X = []
-    N = 2**20
-    A = [-1]*N
+    q = int(input())
+    a = {} # 辞書
+    cnt = {} # 辞書
+    n = 1 << 20
 
-    for _ in range(Q):
+    for _ in [None] * q:
         t, x = map(int, input().split())
-
-        if t == 1:
-            h = x
-            while True:
-                if A[h % N] != -1:
-                    h += 1
-                else:
-                    A[h % N] = x
-                    break
-
-        elif t == 2:
-            print(A[x % N])
-
+        if t == 2:
+            x %= n
+            if x in a:
+                print(a[x])
+            else:
+                print(-1)
+        else:
+            a[index(x)] = x
