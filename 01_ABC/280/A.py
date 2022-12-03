ed3a252fd3a964c6405f3a35994d8585af9b4d9a
GIFFHub <yusuke94ka2nakano@gmail.com>
2022-12-03 21:01:49 +0900

    2022-12-03-01

--- 01_ABC/280/A.py
+++ 01_ABC/280/A.py
@@ -1,3 +1,11 @@
 
 
 if __name__ == '__main__':
+    H, W = map(int, input().split())
+    S = []
+    ans = 0
+    for _ in range(H):
+        s = input()
+        ans += s.count('#')
+
+    print(ans)
