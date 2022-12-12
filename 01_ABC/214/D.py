9fbf12be69d6adde8283db7e5b222d26afbd2737
GIFFHub <yusuke94ka2nakano@gmail.com>
2022-12-13 06:23:37 +0900

    2022-12-11-02

---
+++ 01_ABC/214/D.py
@@ -0,0 +1,14 @@
+
+
+if __name__ == '__main__':
+    N = int(input())
+    U = []
+    V = []
+    W = []
+    for _ in range(N):
+        u, v, w = map(int, input().split())
+        U.append(u)
+        V.append(v)
+        W.append(w)
+
+
