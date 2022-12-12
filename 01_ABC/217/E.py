7a205539dc3838a824144139cf5c7be108b300c9
GIFFHub <yusuke94ka2nakano@gmail.com>
2022-12-13 05:16:41 +0900

    2022-12-11-02

--- 01_ABC/217/E.py
+++ 01_ABC/217/E.py
@@ -1,22 +1,38 @@
 from collections import deque
 import heapq
-if __name__ == '__main__':
-    Q = int(input())
-    A_deque = deque()
-    A_heapq = []
-    heapq.heapify(A_heapq)
 
-    flg = 0
-    for _ in range(Q):
-        query = list(map(int, input().split()))
+Q = int(input())
 
-        if query[0] == 1:
-            A_deque.append(query[1])
-            heapq.heappush(A_heapq, query[1])
-            flg = 1
-        elif query[0] == 2:
-            print(A_deque.popleft())
-            flg = 2
-        else:
+A_d = deque()
+A_h = []
+heapq.heapify(A_h)
 
-            flg = 3
+for i in range(Q):
+  query = list(map(int, input().split()))
+
+  if query[0] == 1:
+    A_d.append(query[1])
+
+  elif query[0] == 2:
+    if len(A_h) == 0:
+      print(A_d.popleft())
+    else:
+      print(heapq.heappop(A_h))
+
+  else:
+    for x in A_d:
+      heapq.heappush(A_h, x)
+    A_d = deque()
+
+  #print('A_d:', A_d)
+  #print('A_h:', A_h)
+
+'''
+考察
+・ヒープとデックを用意する
+・①のとき、デックに追加する
+・③のとき、デックをヒープに入れる
+・②のとき、ヒープの最小値を取り出す。ヒープが空のときっはデックの先頭を取り出す
+
+
+'''
