950ffce784f5e40b6e9174bae656313a5a820ab9
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-01-11 23:12:49 +0900

    2023-01-11-02

--- 01_ABC/222/D3.py
+++ 01_ABC/222/D3.py
@@ -4,25 +4,31 @@ if __name__ == '__main__':
     N = int(input())
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
+    M = max(B)
     ans = 0
     T = 998244353
-    dp = [[0]*(N+1) for _ in range(max(B)+1)]
+    dp = [[0]*(M+1) for _ in range(N)]
+    tmp_crr = [0]*(M+2)
+    tmp = 0
+    for j in range(A[0], M+1):
+        if j <= B[0]:
+            dp[0][j] = 1
+            tmp += 1
+        tmp_crr[j+1] = tmp
 
-    for i in range(A[0], B[0]+1):
-        dp[i][1] = 1
-    for i in range(1, max(B)+1):
-        for j in range(1, N):
-            up = max(i, A[j])
-            down = B[j]
-            for k in range(up, down+1):
-                dp[k][j+1] += dp[i][j]
-                dp[k][j+1] %= T
+    for i in range(1, N):
+        for j in range(A[i], B[i]+1):
+            dp[i][j] = tmp_crr[j+1] - tmp_crr[A[i-1]]
+            dp[i][j] %= T
+        tmp_crr = [0]*(M+2)
+        tmp = 0
+        for j in range(A[i], M+1):
+            tmp += dp[i][j]
+            tmp_crr[j+1] = tmp % T
 
-    for i in range(max(B)+1):
-        ans += dp[i][-1]
-        ans %= T
+    #ans = sum(dp[N-1])
+    ans = sum(dp[N-1]) % T
+
     print(ans)
-
-
 
 
