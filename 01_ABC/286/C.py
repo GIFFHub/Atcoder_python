0f2e341c52705bcdd2c6c09ed590a12720c502a2
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-01-21 21:45:51 +0900

    2023-01-16-02

--- 01_ABC/286/C.py
+++ 01_ABC/286/C.py
@@ -1,5 +1,32 @@
-
-
+from collections import deque
+from collections import defaultdict
 if __name__ == '__main__':
-    pass
+    N, A, B = map(int, input().split())
+    S = input()
+    dq = deque(list(S))
+    T = []
+    d = defaultdict(int)
+    for i in range(N):
+        for j in range(N//2):
+            if dq[j] != dq[N-j-1]:
+                d[i] += 1
+        tmp = dq.popleft()
+        dq.append(tmp)
+        T.append(dq.copy())
+
+    # i : A円払う回数
+    # d[i] : B円払う回数
+
+    ans = 10**9 * 5000 + 1
+    for i in range(N):
+        if i in d:
+            ans = min(ans, A*i + B*d[i])
+        else:
+            ans = min(ans, A*i)
+
+    print(ans)
+
+
+
+
 
