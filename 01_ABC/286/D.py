3d5fe41f4ed526e92964285ed21e631f5dfae709
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-01-21 22:38:38 +0900

    2023-01-16-02

--- 01_ABC/286/D.py
+++ 01_ABC/286/D.py
@@ -1,5 +1,42 @@
 
 
 if __name__ == '__main__':
-    pass
+    N, X = map(int, input().split())
+
+    A = []
+    B = []
+    for _ in range(N):
+        a, b = map(int, input().split())
+        A.append(a)
+        B.append(b)
+
+    '''
+    dp[i][j] : i種類目までの硬貨を使っていい時、合計金額をjにできるか
+    
+    '''
+
+    dp = [[False]*(X+1) for _ in range(N+1)]
+
+    for i in range(N+1):
+        for j in range(X+1):
+            if j == 0:
+                dp[i][j] = True
+
+            if i > 0:
+                if dp[i-1][j]:
+                    dp[i][j] = True
+
+            if dp[i][j]:
+                if i < N:
+                    for b in range(1, B[i]+1):
+                        if j+A[i]*b <= X:
+                            if i <= N-1:
+                                dp[i+1][j+A[i]*b] = True
+
+    if dp[N][X]:
+        print('Yes')
+    else:
+        print('No')
+
+
 
