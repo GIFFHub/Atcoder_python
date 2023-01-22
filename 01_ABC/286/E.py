3be3f40a14091095a77904d9f1de8c301aaa1e10
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-01-22 20:54:22 +0900

    2023-01-16-02

--- 01_ABC/286/E.py
+++ 01_ABC/286/E.py
@@ -1,5 +1,17 @@
 
 
 if __name__ == '__main__':
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+    d = dict()
+    for i in range(N):
+        for j, s in enumerate(input()):
+            if s == 'Y':
+                if i+1 in d:
+                    d[i+1].append(j+1)
+                else:
+                    d[i+1] = [j+1]
+
+    for _ in range(int(input())):
+        u, v = map(int, input().split())
 
