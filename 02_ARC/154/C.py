85e9008a08d066cb6c6c07243f4a6a2fb92caf76
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-01-23 05:09:13 +0900

    2023-01-23-01

---
+++ 02_ARC/154/C.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == '__main__':
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        A = list(map(int, input().split()))
+        B = list(map(int, input().split()))
+
+
+
