2048b4aa10a554c06fe7fe3ffad6debd4f2ce52c
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-01-28 23:14:56 +0900

    2023-01-28-03

--- 01_ABC/145/D.py
+++ 01_ABC/145/D.py
@@ -1,4 +1,10 @@
-from scipy.special import comb
+from math import factorial
+
+def cmb(n, r, p):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % p
 
 
 if __name__ == '__main__':
@@ -10,7 +16,20 @@ if __name__ == '__main__':
         # x方向にa回、y方向にb回移動したとする
         a = X-n
         b = n-a
-        ans = comb(a+b, b, exact=True)
+        ans = factorial
+
+        p = 10 ** 9 + 7
+        N = 10 ** 6  # N は必要分だけ用意する
+        fact = [1, 1]  # fact[n] = (n! mod p)
+        factinv = [1, 1]  # factinv[n] = ((n!)^(-1) mod p)
+        inv = [0, 1]  # factinv 計算用
+
+        for i in range(2, N + 1):
+            fact.append((fact[-1] * i) % p)
+            inv.append((-inv[p % i] * (p // i)) % p)
+            factinv.append((factinv[-1] * inv[-1]) % p)
+
+        ans = cmb(a+b, b, p)
 
     print(ans)
 
---
+++ 98_data_structur/nCr_mod.py
@@ -0,0 +1,21 @@
+# https://blog.satoooh.com/entry/5195/#:~:text=%E7%B5%90%E8%AB%96%E3%81%8B%E3%82%89%E8%A8%80%E3%81%86%E3%81%A8%E3%80%81%20n,(n%E2%88%92r)!)
+
+def cmb(n, r, p):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % p
+
+
+p = 10 ** 9 + 7
+N = 10 ** 6  # N は必要分だけ用意する
+fact = [1, 1]  # fact[n] = (n! mod p)
+factinv = [1, 1]  # factinv[n] = ((n!)^(-1) mod p)
+inv = [0, 1]  # factinv 計算用
+
+for i in range(2, N + 1):
+    fact.append((fact[-1] * i) % p)
+    inv.append((-inv[p % i] * (p // i)) % p)
+    factinv.append((factinv[-1] * inv[-1]) % p)
+
+print(cmb(n, r, p))
