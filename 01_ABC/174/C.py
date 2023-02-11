8c69ea92ab6e8bbff43f1ecfd27d08aa3a235a0f
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-02-11 15:50:14 +0900

    2023-02-11-02

--- 01_ABC/174/C.py
+++ 01_ABC/174/C.py
@@ -1,42 +1,28 @@
 
 
 if __name__ == '__main__':
-    N = int(input())
-    C = input()
-    T = []
+
     '''
-    以下二択
-    　pattern 1 : 左側に白（False）が存在する赤（True)を全て左に持っていく
-    　pattern 2 : 右側に赤（True）が存在する白（False）を全て赤に変える
+    A = [a1, a2, a3,,,,]とした時
+    A[i] % K (B[i]とする) は以下で表せる
+    
+    ・i=1の時
+    　　7 % K
+    ・i>=2の時
+    　　(B[i-1]*10 + 7) % K
+    
+    B[i]が繰り返されると、無限ループに入る
+    0 <= B[i] <= K-1であり、K回繰り返せば、必ず複数回同じ値のものがでてくる（鳩の巣原理）
+    よってO(K)でよい
     '''
-    for c in C:
-        if c == 'R':
-            T.append(True)
-        else:
-            T.append(False)
-
-    # 左側に白が存在する赤の数を数える
-    flg = 0
-    pattern1 = 0
-    for t in T:
-        if not t:
-            flg = 1
-        if flg and t:
-            pattern1 += 1
-    for i in range(pattern1):
-        if T[i]:
-            pattern1 -= 1
-
-    # 右側に赤が存在する白の数を数える
-    flg = 0
-    pattern2 = 0
-    T.reverse()
-    for t in T:
-        if t:
-            flg = 1
-        if flg and not t:
-            pattern2 += 1
-
-    print(min(pattern1, pattern2))
-
+    K = int(input())
+    num = 0
+    for i in range(K+1):
+        num = (10 * num + 7) % K
+        if num == 0:
+            ans = i+1
+            break
+    else:
+        ans = -1
+    print(ans)
 
