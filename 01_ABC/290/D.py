307cf197d615e62521f18047f83a7a2f6db09b7f
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-02-20 06:23:36 +0900

    2023-02-20-01

--- 01_ABC/290/D.py
+++ 01_ABC/290/D.py
@@ -1,43 +1,21 @@
-
+import math
 
 if __name__ == '__main__':
     T = int(input())
     for _ in range(T):
         N, D, K = map(int, input().split())
-        if D > N:
-            D %= N
-        '''
-        ・一度塗れたら、最後までずっと塗れる
-        '''
+        g = math.gcd(N, D)
 
-        T = [False] * N
-        T[0] = True
-        # 塗った回数
-        cnt = 1
-        if cnt == K:
-            print(0)
-            continue
+        n = N // g
+        # n回でスタートラインに返ってくる
+        # K回のうちに何回スタートラインに戻ってきたか
+        # →その分右に1ずれる
+        cir = (K-1) // n # 0地点に印をつけるためK-1
 
-        start_point = 0
-        second_point = (start_point + D) % N
-        while True:
-            # 2箇所目
-            # 2箇所目が塗られている限り
-            if T[second_point]:
-                second_point = (second_point + 1) % N
-            cnt += 1
-            if cnt == K:
-                ans = second_point
-                break
-            # 今回second_pointを起点にして塗る量だけ足したらK番目を超える場合
-            # 次に塗れる回数
-            amount = (N-second_point-1) // D
-            if K <= cnt + amount:
-                ans = second_point + (K-cnt)*D
-                break
-            # 超えない場合
-            else:
-                cnt += amount
-                second_point = (second_point + D*(amount+1)) % N
-        print(ans)
+        # 最後の1周でどこまでいくか
+        last = (K-1) * D % N
+
+        print(cir+last)
+
+
 
