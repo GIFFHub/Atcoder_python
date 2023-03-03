deed081b42f8da816c7936abecfeff586dc4d03f
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-03-04 06:47:08 +0900

    2023-03-04-01

--- 98_data_structur/nCr_mod_largen.py
+++ 98_data_structur/nCr_mod_largen.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+
 def cmb(n, r, p):
     r = min(n - r, r)
     if r == 0:
@@ -8,9 +9,5 @@ def cmb(n, r, p):
     denom = reduce(lambda x, y: (x*y)%p, range(1, r + 1))
     return (numer * pow(denom, p-2, p)) % p
 
-p = 10 ** 9 + 7
-n, a, b = map(int, input().split())
 
-ans = pow(2, n, p) - 1
-ans -= cmb(n, a, p) + cmb(n, b, p)
-print(ans % p)
+p = 10 ** 9 + 7
