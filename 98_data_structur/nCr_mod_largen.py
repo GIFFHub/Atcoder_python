be31467bde9e7a36fe61ec2d537237fcd4311790
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-03-04 20:56:24 +0900

    2023-03-04-01

--- 98_data_structur/nCr_mod_largen.py
+++ 98_data_structur/nCr_mod_largen.py
@@ -2,11 +2,17 @@ from functools import reduce
 
 
 def cmb(n, r, p):
+    # nCr と n-rCrは同じなので小さい方にする
     r = min(n - r, r)
+    # rが0なら必ず1
     if r == 0:
         return 1
+
+    # 分子
     numer = reduce(lambda x, y: (x*y)%p, range(n, n - r, -1))
+    # 分母
     denom = reduce(lambda x, y: (x*y)%p, range(1, r + 1))
+
     return (numer * pow(denom, p-2, p)) % p
 
 
