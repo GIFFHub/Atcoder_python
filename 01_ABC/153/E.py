f4840fb2fc28e2b26fe7e794bdbdfd2499071f05
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-03-17 21:12:23 +0900

    2023-03-17-01

--- 01_ABC/153/E.py
+++ 01_ABC/153/E.py
@@ -2,9 +2,13 @@
 
 if __name__ == '__main__':
     H, N = map(int, input().split())
-    for _ in range(N):
-        a, b = map(int, input().split())
+    AB = [tuple(map(int, input().split())) for _ in range(N)]
 
-    # dp[i][j] i番目までの魔法を使って体力をj以下にする際の最大残り魔力
+    dp = [10**10] * (H + 1)
+    dp[H] = 0
 
+    for a, b in AB:
+        for h in range(H+1)[::-1]:
+            dp[h] = min(dp[h], dp[min(H, h+a)] + b)
+    print(dp[0])
 
