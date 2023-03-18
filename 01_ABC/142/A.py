23ec58d3be039a3b9299dfa20875fad46c437b47
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-03-19 05:35:08 +0900

    2023-03-19-01

---
+++ 01_ABC/142/A.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == '__main__':
+    N = int(input())
+    if N % 2 == 0:
+        ans = 0.5
+    else:
+        ans = (N//2 + 1) / N
+
+    print(ans)
---
+++ 01_ABC/142/C.py
@@ -0,0 +1,9 @@
+
+
+if __name__ == '__main__':
+    N = int(input())
+    A = list(map(int, input().split()))
+    ans = [0]*N
+    for i, a in enumerate(A):
+        ans[a-1] = i+1
+    print(*ans)
