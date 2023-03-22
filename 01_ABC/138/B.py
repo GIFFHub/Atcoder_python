79798a768593f8f52dc6809b83e4a0334ed33e0e
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-03-23 05:27:13 +0900

    2023-03-23-01

---
+++ 01_ABC/138/B.py
@@ -0,0 +1,10 @@
+
+
+if __name__ == '__main__':
+    N = int(input())
+    A = list(map(int, input().split()))
+    s = 0
+    for a in A:
+        s += 1/a
+
+    print(1/s)
---
+++ 01_ABC/138/C.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == '__main__':
+    N = int(input())
+    V = list(map(int, input().split()))
+    V.sort()
+    ans = V[0]
+    for i in range(1, N):
+        ans = (ans + V[i])/2
+
+    print(ans)
