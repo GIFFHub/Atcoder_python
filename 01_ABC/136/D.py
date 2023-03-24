a75327b3639410d0c1149aea8f2417dd864e296d
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-03-25 06:37:58 +0900

    2023-03-25-01

--- 01_ABC/136/D.py
+++ 01_ABC/136/D.py
@@ -1,5 +1,21 @@
-
+import bisect
 
 if __name__ == '__main__':
 
     S = input()
+    N = len(S)
+    point = []
+    for i in range(N-1):
+        if S[i] == 'R' and S[i+1] == 'L':
+            point.append(i)
+
+    ans = [0]*N
+    for i, s in enumerate(S):
+        terminal = bisect.bisect_left(point, i)
+        if s == 'L':
+            terminal -= 1
+        move = abs(point[terminal] - i)
+        ans[point[terminal] + move % 2] += 1
+    print(*ans)
+
+
