71bb5ca00aab13f6769f9232e1fe0c801a03c97e
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-03-26 05:54:24 +0900

    2023-03-26-01

--- 01_ABC/149/D.py
+++ 01_ABC/149/D.py
@@ -3,4 +3,21 @@
 if __name__ == '__main__':
     N, K = map(int, input().split())
     R, S, P = map(int, input().split())
-    T = int(input())
+    d = dict()
+    d['r'] = P
+    d['s'] = R
+    d['p'] = S
+    A = input()
+    T = []
+    for a in A:
+        T.append(a)
+
+    point = 0
+    for i in range(N):
+        if i >= K:
+            if T[i] == T[i-K]:
+                T[i] = 'x'
+                continue
+        point += d[T[i]]
+
+    print(point)
