8e95fbd3d7a43180df110a424037ef3a85e5ace8
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-08-07 23:38:11 +0900

    2023/08/07-1

--- 01_ABC/312/B.py
+++ 01_ABC/312/B.py
@@ -1,3 +1,22 @@
 
 
 if __name__ == '__main__':
+    N, M = map(int, input().split())
+    S = []
+    for _ in range(N):
+        S.append(input())
+
+    for y in range(N):
+        for x in range(M):
+            if x <= M-9 and y <= N-9:
+                if S[y][x:x+4] == '###.':
+                    if S[y+1][x:x+4] == '###.':
+                        if S[y+2][x:x+4] == '###.':
+                            if S[y+3][x:x+4] == '....':
+                                if S[y+5][x+5:x+9] == '....':
+                                    if S[y+6][x+5:x+9] == '.###':
+                                        if S[y+7][x+5:x+9] == '.###':
+                                            if S[y+8][x+5:x+9] == '.###':
+                                                print(y+1, x+1)
+
+
