c1f085135b1e1cc5483263083189e959bba93d7d
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-11-11 09:25:28 +0900

    2023/11/10-ABC/319/B

---
+++ 01_ABC/319/B.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == '__main__':
+    N = int(input())
+    for i in range(N+1):
+        for j in range(1, 10):
+            if i % (N/j) == 0:
+                print(j, end='')
+                break
+        else:
+            print('-', end='')
+
