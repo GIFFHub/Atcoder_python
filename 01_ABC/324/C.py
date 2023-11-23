a858849f29a62c3b26a2447a214e21634dfbc748
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-11-23 09:48:07 +0900

    2023/11/10-ABC/324/C

--- 01_ABC/324/C.py
+++ 01_ABC/324/C.py
@@ -1,57 +1,54 @@
 if __name__ == '__main__':
     N, T = input().split()
     N = int(N)
-    K = []
+
     len_T = len(T)
-    for i in range(1, N+1):
+    ans = []
+    for i in range(N):
         S = input()
 
         if S == T:
-            K.append(i)
+            ans.append(i+1)
         else:
-            flg = 0
             len_S = len(S)
-            # 一文字挿入した可能性があるもの
-            if len_T == len_S + 1:
+
+            if len_S + 1 == len_T:
                 for j in range(len_S):
-                    if flg == 0:
-                        if S[j] != T[j]:
-                            if S[j] == T[j+1]:
-                                flg = 1
-                            else:
-                                break
-                    else:
-                        if S[j] != T[j+1]:
+                    if S[j] != T[j]:
+                        if T[:j] + T[j+1:] == S:
+                            ans.append(i+1)
+                            break
+                        else:
                             break
                 else:
-                    K.append(i)
-            # 一文字変更した可能性があるもの
-            elif len_T == len_S:
-                for j in range(len_S):
-                    if flg == 0:
-                        if S[j] != T[j]:
-                            flg = 1
-                    else:
-                        if S[j] != T[j]:
-                            break
-                else:
-                    K.append(i)
-            # 一文字削除した可能性があるもの
-            else:
+                    ans.append(i+1)
+
+            elif len_S - 1 == len_T:
                 for j in range(len_T):
-                    if flg == 0:
-                        if S[j] != T[j]:
-                            if S[j+1] == T[j]:
-                                flg = 1
-                            else:
-                                break
-                    else:
-                        if S[j+1] != T[j]:
+                    if S[j] != T[j]:
+                        if S[:j] + S[j+1:] == T:
+                            ans.append(i+1)
+                            break
+                        else:
                             break
                 else:
-                    K.append(i)
+                    ans.append(i+1)
+
+            elif len_S == len_T:
+                flg = 0
+                for j in range(len_S):
+                    if S[j] != T[j]:
+                        if flg == 0:
+                            flg = 1
+                        else:
+                            break
+                else:
+                    ans.append(i+1)
+    print(len(ans))
+    print(*ans)
 
 
-    print(len(K))
-    print(*K)
+
+
+
 
