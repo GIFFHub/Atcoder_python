47f2f2084ca8ec44d56c66841d8cf73d25dd66f5
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-11-25 21:00:54 +0900

    2023/11/10-ABC/330/A

---
+++ 01_ABC/330/A.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    N, L = map(int, input().split())
+    A = list(map(int, input().split()))
+    ans = 0
+    for a in A:
+        if a >= L:
+           ans += 1
+
+    print(ans)
