3dad2f2ed87a5c7a735da07e4c74a2d2f83a0e3d
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-11-26 08:26:01 +0900

    2023/11/26/ABC330/C

--- 01_ABC/330/C.py
+++ 01_ABC/330/C.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     ans = 1414213123123412
     T = []
-    for x in range(0, 1500000):
+    for x in range(0, 1414214):
         if x**2 > D:
             break
         T.append(x**2)
@@ -17,18 +17,10 @@ if __name__ == '__main__':
     for i in range(len_T):
         x2 = T[i]
         y2_index = bisect.bisect(T, D-x2)
-        if y2_index - 3 >= 0:
-            ans = min(ans, abs(x2 + T[y2_index-3] - D))
-        if y2_index - 2 >= 0:
-            ans = min(ans, abs(x2 + T[y2_index-2] - D))
         if y2_index - 1 >= 0:
             ans = min(ans, abs(x2 + T[y2_index-1] - D))
         if y2_index < len_T:
             ans = min(ans, abs(x2 + T[y2_index] - D))
-        if y2_index + 1 < len_T:
-            ans = min(ans, abs(x2 + T[y2_index+1] - D))
-    if ans < 0:
-        ans = 0
     print(ans)
 
 
