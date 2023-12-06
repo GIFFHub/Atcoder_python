1a9ee25ed0c78d14f9487958efcd440c1b0dc7e4
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-12-06 22:27:51 +0900

    2023/11/26/tenkei90/14

---
+++ 06_Tenkei90/14.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    A.sort()
+    B.sort()
+    ans = 0
+    for a, b in zip(A, B):
+        ans += abs(a-b)
+    print(ans)
