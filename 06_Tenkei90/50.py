129ea7376a720c8e587232431ffdbd1c53ea50ab
GIFFHub <yusuke94ka2nakano@gmail.com>
2023-12-08 17:27:16 +0900

    2023/11/26/tenkei90/50_1

--- 06_Tenkei90/50.py
+++ 06_Tenkei90/50.py
@@ -4,9 +4,8 @@ if __name__ == '__main__':
     # dp[i] : i段目にたどり着くまでに何通りあるか
     dp = [0] * (N+1)
     dp[0] = 1
-    dp[1] = 1
 
-    for i in range(2, N+1):
+    for i in range(1, N+1):
         if i-L >= 0:
             dp[i] = dp[i-1]+dp[i-L]
         else:
